11dfb60ce94fe36b85227774f7393ddcbc64a8ba
sayem499 <sayem499@gmail.com>
2023-11-13 13:46:37 +0600

    Configured CORS for multiple origin in backend.

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,7 @@ connectMongoDB();
 const port = process.env.PORT || 8000;
 
 const app = express();
-
-app.use(cors({origin: process.env.ORIGIN, credentials: true}));
+app.use(cors({origin: [process.env.ORIGIN_1, process.env.ORIGIN_2], credentials: true}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
