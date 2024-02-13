1e2d8161d6da8cb3d0d9a1bae93d211dd5ca5717
sayem499 <sayem499@gmail.com>
2024-02-14 00:10:12 +0600

    Created pressable buttons for picked and delivered parcel screens.

--- mobile-app/courier-next-app/views/homeScreen.js
+++ mobile-app/courier-next-app/views/homeScreen.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, Pressable } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 const logoImg = require('../assets/fast-delivery-truck.png');
 
@@ -20,7 +21,20 @@ const HomeScreen = ({navigation}) => {
           <Image source={logoImg} style={styles.logoImage} />
         </View>
         <View style={styles.homescreenInner_container}>
+          <View style={styles.homescreenInner_1stRow}>
+            <Pressable style={styles.homescreen_pressable}>
+              <FontAwesome5 style={styles.homescreenImage_pressable} name="box" size={40} color="blue" />
+              <Text style={styles.homescreenText_pressable}>Picked{'\n'}Parcels</Text>
+            </Pressable>
+            
+            <View style={styles.boxShadow}>
+              <Pressable style={styles.homescreen_pressable}>
+                <FontAwesome5 style={styles.homescreenImage_pressable} name="box-open" size={40} color="green" />
+                <Text style={styles.homescreenText_pressable}>Delivered{'\n'}Parcels</Text>
+              </Pressable>
+            </View>
 
+          </View>
         </View>
     </View>
   )
@@ -48,8 +62,35 @@ const styles = StyleSheet.create({
      },
 
     homescreenInner_container: { 
-      width: '90%', height: '75%', backgroundColor: 'white', borderRadius: 10, marginBottom: 60,
+      width: '90%', height: '75%', backgroundColor: 'white', borderRadius: 10, marginBottom: 40,
     },
+
+    homescreenInner_1stRow: {
+     justifyContent: 'space-around', alignItems: 'center', flexDirection: 'row', marginTop: 50,
+    },
+
+    homescreen_pressable: { 
+      alignItems: 'center', justifyContent: 'center',
+    },
+
+    homescreenImage_pressable: {
+      marginTop: 1, marginBottom: 1,
+    },
+
+    homescreenText_pressable: {
+      textAlign: 'center', marginTop: 5,
+    },
+
+    boxShadow: {
+      shadowColor: '#0c94f5',
+      shadowOffset: {
+        width: 0,
+        height: 2,
+      },
+      shadowOpacity: 0.7,
+      elevation: 10,
+    },
+
 
     
 })
