1279335ff01493ecd7c026ea97fdf5d0c6423ef3
sayem499 <sayem499@gmail.com>
2024-02-19 14:02:26 +0600

    Created create admin and deliveryman components.

--- frontend/courier-next-app/app/admin/admin/page.tsx
+++ frontend/courier-next-app/app/admin/admin/page.tsx
@@ -1,80 +1,51 @@
 "use client"
+import React, { useState } from 'react'
+import Createadmin from '@/components/create_admin';
 
-import Divisions from '@/JSON/bd-divisions.json';
-import React, {useState} from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import { toast } from 'react-toastify';
-import { useRegisterAdminMutation } from '@/redux/admin/adminApiSlice';
 
-const Createadmin = () => {
-    const _id = uuidv4();
-    const [registerAdmin] = useRegisterAdminMutation(); 
-    const [admin_email, setEmail] = useState('');
-    const [admin_password, setPassword] = useState('');
-    const [admin_passwordConfirm, setPasswordConfirm] = useState('');
-    const [admin_location, setLocation] = useState('');
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if(admin_password === admin_passwordConfirm){
-        try{
-            const res = await registerAdmin({ _id, admin_email, admin_password, admin_location }).unwrap();
-            if(res){
-                toast.success('Admin registered successfully');
-                setEmail('');
-                setPassword('');
-                setPasswordConfirm('');
-                setLocation('');
-            }
-        }catch(err: any){
-            toast.error(err.data.message || err.error);
-        }
-    }else {
-       toast.error('Passwords do not match.'); 
-    }
-  }  
+
+const Admin = () => {
+  const [stepOne, setStepOne] = useState(true);
+  const [stepTwo, setStepTwo] = useState(false);
+  const [stepThree, setStepThree] = useState(false);
+
+
+  const stepOneSelected = () => {
+    setStepOne(true);
+    setStepTwo(false);
+    setStepThree(false);
+  }
+  
+  const stepTwoSelected = () => {
+    setStepOne(false);
+    setStepTwo(true);
+    setStepThree(false);
+  }
+  
+  const stepThreeSelected = () => {
+    setStepOne(false);
+    setStepTwo(false);
+    setStepThree(true);
+  }
 
   return (
     <div className='flex flex-col w-[100%] h-[100%] '>
-     <div className='flex w-[100%] h-[30%]'>
-     </div>
+      <div className='flex w-[100%] h-[10%] items-center justify-center'>
+        <div className='flex h-[100%] w-[100%] items-center justify-center'>
+          <button onClick={stepOneSelected} className={`m-2 h-fit w-fit ${stepOne ? 'border-b-4  border-b-blue-500  dark:border-b-indigo-500' : ''}`}>Create</button>
+          <button onClick={stepTwoSelected} className={`m-2 h-fit w-fit ${stepTwo ? 'border-b-4  border-b-blue-500  dark:border-b-indigo-500' : ''}`}>Update</button>
+          <button onClick={stepThreeSelected} className={`m-2 h-fit w-fit ${stepThree ? 'border-b-4  border-b-blue-500  dark:border-b-indigo-500' : ''}`}>Delete</button>
+        </div>
+      </div>
+      <div className='flex w-[100%] h-[90%] justify-center'>
+        {
+          stepOne && <Createadmin/>
+        }
+      </div>
 
-     <div className='flex w-[100%] h-[70%] justify-center'>
-        <form className='flex flex-col w-[30%] items-center' onSubmit={handleSubmit}>
-            <div className='flex flex-col mb-2 w-[100%]'>
-                <label htmlFor='admin_email' className='mb-1 '>Email</label>
-                <input value={admin_email} onChange={(e) => setEmail(e.target.value)} type='email' className='text-black rounded-sm p-2 text-sm' id='admin_email' name='admin_email' placeholder='Enter email'/>
-            </div>  
-
-            <div className='flex flex-col mb-2 w-[100%]'>
-                <label htmlFor='admin_password' className='mb-1 '>Password</label>
-                <input value={admin_password} onChange={(e) => setPassword(e.target.value)} type='password' className='text-black rounded-sm p-2 text-sm' id='admin_password' name='admin_password' placeholder='Enter password'/>
-            </div>
-
-            <div className='flex flex-col mb-2 w-[100%]'>
-                <label htmlFor='admin_passwordConfirm' className='mb-1 '>Re-type Password</label>
-                <input value={admin_passwordConfirm} onChange={(e) => setPasswordConfirm(e.target.value)} type='password' className='text-black rounded-sm p-2 text-sm' id='admin_passwordConfirm' name='admin_passwordConfirm' placeholder='Confirm password'/>
-            </div>
-
-            <div className='flex flex-col mb-2 w-[100%]'>
-                <label htmlFor='admin_location' className='mb-1 '>Location</label>
-                <select value={admin_location} onChange={(e) => setLocation(e.target.value)} className='text-black rounded-sm p-2 text-sm' id='admin_location' name='admin_location' placeholder='Enter location'>
-                <option value={''}>Choose Location</option>
-                {
-                   Divisions.divisions.map((div, key) => {
-                    return(
-                        <option key={key} id={div.id} value={div.name}>{div.name}</option>
-                    )
-                   }) 
-                }
-                </select>
-            </div>
-
-            <button type='submit' className=' mt-5 text-white rounded-sm bg-green-500 w-[40%] h-[6%]'>Submit</button>  
-        </form>
-     </div>
     </div>
   )
 }
 
-export default Createadmin
+export default Admin
---
+++ frontend/courier-next-app/components/create_deliveryman.tsx
@@ -0,0 +1,90 @@
+"use client"
+import React, { useState } from 'react'
+import { useAppSelector, useAppDispatch } from '@/app/hooks';
+import { useRouter } from 'next/navigation';
+import { useSetDeliveryManMutation } from '@/redux/deliveryMan/deliveryManAdminApiSlice';
+import { setDeliveryManData } from '@/redux/deliveryMan/deliveryManAdminSlice';
+import { v4 as uuidv4 } from 'uuid';
+import { toast } from 'react-toastify';
+
+const Createdeliveryman = () => {
+  const [ setDeliveryMan ]= useSetDeliveryManMutation();
+  const dispatch = useAppDispatch();
+  const { deliveryman } = useAppSelector(state => state.deliveryManState);
+  const [ deliveryMan_username, setUsername ] = useState('');
+  const [ deliveryMan_phonenumber, setPhonenumber ] = useState('');
+  const [ deliveryMan_email, setEmail ]= useState('');
+  const [ deliveryMan_password, setPassword ] = useState('');
+  const [ deliveryMan_confirmPassword, setConfirmPassword ] = useState('');
+  const _id = uuidv4();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if( deliveryMan_password === deliveryMan_confirmPassword){
+    try{ 
+     const res = await setDeliveryMan({ _id, deliveryMan_username, deliveryMan_phonenumber, deliveryMan_email, deliveryMan_password }).unwrap();
+     dispatch(setDeliveryManData(res));
+     if(res){
+      toast.success('Deliveryman Created Successfully!');
+      setUsername('');
+      setPhonenumber('');
+      setEmail('');
+      setPassword('');
+      setConfirmPassword('');
+     }
+    }catch(err: any){
+      toast.error( err.data.message || err.error );
+    }
+   } else {
+     toast.error('Passwords do not match!');
+   }
+  }
+
+  return (
+    <div className='flex flex-col h-[100%] w-[100%]'>
+      <div className='flex h-[100%] w-[100%] justify-center'>
+            <div className='h-[100%] w-[80%] items-center '>
+              <form className='flex flex-col items-center justify-center' onSubmit={(e)=>handleSubmit(e)}>
+
+                <span className='text-bold text-2xl'>Deliveryman Registration</span>   
+
+                <div className='flex w-[50%] flex-col mb-5'>
+                 <label className='mb-1' htmlFor='deliveryMan_username'>Username</label>
+                 <input onChange={(e)=> setUsername(e.target.value)} value={deliveryMan_username} className='rounded-sm text-sm p-2 text-black' placeholder='Enter Username' type="text" name='deliveryMan_username' id='deliveryMan_username'/>
+                </div>
+
+                <div className='flex w-[50%] flex-col mb-5'>
+                 <label className='mb-1' htmlFor='deliveryMan_phonenumber'>Phonenumber</label>
+                 <input onChange={(e)=> setPhonenumber(e.target.value)} value={deliveryMan_phonenumber} className='rounded-sm text-sm p-2 text-black' placeholder='Enter Phonenumber'  type="text" name='deliveryMan_phonenumber' id='deliveryMan_phonenumber'/>
+                </div>
+
+                <div className='flex w-[50%] flex-col mb-5'>
+                 <label className='mb-1' htmlFor='deliveryMan_email'>Email</label>
+                 <input onChange={(e)=> setEmail(e.target.value)} value={deliveryMan_email} className='rounded-sm text-sm p-2 text-black' placeholder='Enter Email'  type="text" name='deliveryMan_email' id='deliveryMan_email'/>
+                </div>
+
+                <div className='flex w-[50%] flex-col mb-5'>
+                 <label className='mb-1' htmlFor='deliveryMan_image'>Image</label>
+                 <input accept='.jpeg, .png, .jpg' className='rounded-sm text-sm p-2 text-black'  type="file" name='deliveryMan_image' id='deliveryMan_image'/>
+                </div>
+
+                <div className='flex w-[50%] flex-col mb-5'>
+                 <label className='mb-1' htmlFor='deliveryMan_password'>Password</label>
+                 <input onChange={(e)=> setPassword(e.target.value)} value={deliveryMan_password} className='rounded-sm text-sm p-2 text-black' placeholder='Enter Password'  type="password" name='deliveryMan_password' id='deliveryMan_password'/>
+                </div>
+
+                <div className='flex w-[50%] flex-col mb-5'>
+                 <label className='mb-1' htmlFor='deliveryMan_confirmPassword'>Re-type Password</label>
+                 <input onChange={(e)=> setConfirmPassword(e.target.value)} value={deliveryMan_confirmPassword} className='rounded-sm text-sm p-2 text-black' placeholder='Confirm Password'  type="password" name='deliveryMan_confirmPassword' id='deliveryMan_confirmPassword'/>
+                </div>
+                
+                <button type='submit' className='w-[15%] rounded-md p-2 bg-green-500 text-white'>Submit</button>
+              </form>
+
+            </div>
+      </div>
+    </div>
+  )
+}
+
+export default Createdeliveryman
